e4d4474a8c600510a3815cebd695e1888d93d9c9
Barret Schloerke <schloerke@gmail.com>
2011-01-12 15:23:55 -0800

    commented error causing code

--- R/skeleton.R
+++ R/skeleton.R
@@ -181,29 +181,29 @@ make_view_interaction <- function(path, func_c) {
 	print_line("  <title><%= header %></title>", file_path = file_path)
 	print_line("</head>", file_path = file_path)
 	print_line("<script type=\"text/javascript\" src=\"http://code.jquery.com/jquery-latest.min.js\">", file_path = file_path)
-	<script type="text/javascript">
-	
-		$(document).ready(function() {
-			
-		});
-		
-		execute_, func_c,(){
-			
-			$.ajax({
-			    url: 'document.xml',
-			    type: 'GET',
-			    dataType: 'xml',
-			    timeout: 1000,
-			    error: function(){
-			        alert('Error loading XML document');
-			    },
-			    success: function(xml){
-			        // do something with xml
-			    }
-			});
-		}
-	
-	</script>
+	# <script type="text/javascript">
+	# 
+	# 	$(document).ready(function() {
+	# 		
+	# 	});
+	# 	
+	# 	execute_, func_c,(){
+	# 		
+	# 		$.ajax({
+	# 		    url: 'document.xml',
+	# 		    type: 'GET',
+	# 		    dataType: 'xml',
+	# 		    timeout: 1000,
+	# 		    error: function(){
+	# 		        alert('Error loading XML document');
+	# 		    },
+	# 		    success: function(xml){
+	# 		        // do something with xml
+	# 		    }
+	# 		});
+	# 	}
+	# 
+	# </script>
 	
 	
 	print_line("<body>", file_path = file_path)
