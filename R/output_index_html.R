7cd47fb54c3fb0d3574025b563d65ab4a98c9cdf
Barret Schloerke <schloerke@gmail.com>
2011-01-15 17:13:05 -0800

    changed index to utilize new functions

--- R/output_index_html.R
+++ R/output_index_html.R
@@ -8,14 +8,17 @@ make_index_html <- function(path) {
 <p>The orientation of the function has been switched around to accomodate html requests.</p>
 <p>The new format involves setting the arguments as url query variables such as:</p>
 
-<pre>example_seq(to = 10)
+<pre>body_text(\"body_text\")
 to
-http://127.0.0.1:8181/<b>example_seq?to=10</b>
-http://bigbear-raconteur.metamx.com/raconteur/example_seq/<b>example_seq?to=10</b>
+http://127.0.0.1:8181/<b>body_text?func_c=body_text</b>
+http://bigbear-raconteur.metamx.com/raconteur/example_skeleton/<b>body_text?func_c=body_text</b></pre>
 
-example_seq(from = 4, to = 7)
-to
-http://bigbear-raconteur.metamx.com/raconteur/example_seq/<b>example_seq?from=4&to=7</b></pre>"
+<p>A function to help you generate your url is \"func_to_url().\"</p>
+<pre>> func_to_url(body_text) #func_to_url(\"body_text\")
+[1] body_text?func_c=<obj>
+
+> func_to_url(body_text, url = \"http://bigbear-raconteur.metamx.com/raconteur/example_skeleton\")
+[1] \"http://bigbear-raconteur.metamx.com/raconteur/example_skeleton/body_text?func_c=<obj>\""
 
 	cat(content, file = file.path(path, "views", "index.html"))
 }
