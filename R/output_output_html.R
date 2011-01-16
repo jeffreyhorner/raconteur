177516694869f12f9578108c61c4d440bcef69e7
Barret Schloerke <schloerke@gmail.com>
2011-01-15 17:13:38 -0800

    made output_html much easier to read in R code

--- R/output_output_html.R
+++ R/output_output_html.R
@@ -5,14 +5,14 @@ make_view_output_html <- function(path) {
 	print_line("", file_path = file_path, append = FALSE)
 	
 	
-	print_line("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">", file_path = file_path)
-	print_line("<html xmlns=\"http://www.w3.org/1999/xhtml\">", file_path = file_path)
-	print_line("<head>", file_path = file_path)
-	print_line("  <title><%= header %></title>", file_path = file_path)
-	print_line("</head>", file_path = file_path)
-	print_line("<body>", file_path = file_path)
-	print_line("	<%= output %>", file_path = file_path)
-	print_line("</body>", file_path = file_path)
-	print_line("</html>", file_path = file_path)
+	print_line("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">
+<html xmlns=\"http://www.w3.org/1999/xhtml\">
+	<head>
+	  <title><%= header %></title>
+	</head>
+	<body>
+		<%= output %>
+	</body>
+</html>", file_path = file_path)
 	
 }
