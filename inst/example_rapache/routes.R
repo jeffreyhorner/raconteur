709fbab4155227e1365b41f842cb22c87883cf98
Barret Schloerke <schloerke@gmail.com>
2011-01-19 11:10:06 -0800

    moving all rendering functions to a file

--- inst/example_rapache/routes.R
+++ inst/example_rapache/routes.R
@@ -46,41 +46,6 @@ router$get("/dataset/:dataset.csv", function(dataset) {
 })
 
 
-# Convenience function to render an R object to JSON
-# Supposed to be part of sinartra, but not yet.
-render_json <- function(object) {
-
-	# Some objects are instances of a sub-class of data.frame
-	# and RJSONIO doesn't know what to do with them, so we just
-	# use trickery.
-	if (inherits(object,'data.frame',which=TRUE) > 0){
-
-		class(object) <- 'data.frame'
-
-		# Even these may have ts objects as columns so lets 
-		# just punt for right now and assign it an NA column.
-		for (i in names(object)){
-			if (inherits(object[[i]],'ts')){
-				object[[i]] <- NA
-			}
-		}
-	}
-
-	# Unclassing here is unclassy. Would be nice to use as.data.frame
-	# but it seems that the original ts object is stuffed into the result
-	# somehow.
-	if (inherits(object,'ts')){
-		object <- unclass(object)
-	}
-
-	if (inherits(object,'table') || inherits(object,'array')){
-		object <- as.data.frame(object)
-	}
-
-	setContentType('application/json')
-	cat(toJSON(object))
-	OK
-}
 
 ###########################################
 # Web services
