9b76f9a3ae5afa984a276dc1a34cd794eda005d5
Barret Schloerke <schloerke@gmail.com>
2011-01-20 15:22:04 -0800

    removed the sourcing of the test functions

--- load.r
+++ load.r
@@ -17,7 +17,7 @@ PATH <- dirname(FILE)
 # if (!exists("flea")) load(file.path(PATH, "data", "flea.rda"))
 
 lapply(dir(file.path(PATH, "R"), full.name=TRUE), source)
-lapply(dir(file.path(PATH, "test_funcs"), full.name=TRUE), source)
+# lapply(dir(file.path(PATH, "test_funcs"), full.name=TRUE), source)
 
 if(tools:::httpdPort == 0L) {
 	# options(help.ports = 8181)
